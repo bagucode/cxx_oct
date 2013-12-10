c4ada50d157f2fa736277ccec34c2a4ded15ecb8
Markus Gustavsson <markus.gustavsson@sbsdiscovery.se>
2013-12-10 15:32:20 +0100

    what am I doing?

--- src/t2.cpp
+++ src/t2.cpp
@@ -114,4 +114,79 @@ typedef void* Address;
 
 #endif
 
+template <typename T, Uword size = 0>
+struct Array {
+	typedef typename T ElementType;
+
+	T data[size];
+};
+
+template <typename T>
+struct Array<T, 0> {
+	typedef typename T ElementType;
+
+	Uword size;
+	T* data;
+};
+
+struct Type;
+
+struct String {
+	Uword size;
+	const char* data; // TODO: Proper string. Probably use OS string types, they have unicode support.
+};
+
+struct Field {
+	Type* type;
+	Uword kind;
+	Uword offset;
+	String name;
+};
+
+struct Type {
+	Uword size;
+	Uword alignment;
+	Array<Field> fields;
+};
+
+struct TypeProtocol {
+	typedef Type SelfType;
+
+	Uword(*getSize)(Type* self);
+	Uword(*getAlignment)(Type* self);
+	Array<Field>(*getFields)(Type* self);
+};
+
+template <typename PFns>
+struct VTable {
+	Type* type;
+	PFns fns;
+};
+
+template <typename PFns>
+struct ProtocolObject {
+	typename PFns::SelfType* self;
+	VTable<PFns>* vtable;
+};
+
+
+#define call(Function, ProtocolObject, ...) ProtocolObject.vtable->fns.Function(ProtocolObject.self, __VA_ARGS__)
+
+#include <iostream>
+
+static Uword testFn(Type* ti) {
+	return ti->alignment;
+}
+
+int main() {
+	ProtocolObject<TypeProtocol> aProtocolObject;
+	Type t1;
+	t1.alignment = sizeof(Uword);
+	aProtocolObject.self = &t1;
+	VTable<TypeProtocol> vt1;
+	vt1.fns.getAlignment = testFn;
+	aProtocolObject.vtable = &vt1;
+	std::cout << "Alignment: " << call(getAlignment, aProtocolObject) << std::endl;
+}
+
 
